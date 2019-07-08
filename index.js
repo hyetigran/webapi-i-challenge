93828cd2b7777c97aee58751e8117dda15db1d88
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-08 16:30:44 +0400

    added crud requests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // implement your API here
+const express = require("express");
+const server = express();
+const Db = require("./data/db");
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  Db.find()
+    .then(data => {
+      console.log("happy path");
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+server.listen(5000, () => {
+  console.log("listening on 5000");
+});
